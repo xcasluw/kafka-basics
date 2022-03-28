eb39291ca4bbcdac770d9490433e0112253c4901
Lucas <oligena@hotmail.com>
2022-03-28 16:17:46 -0300

    readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Node + Kafka (Basics)
+
+A kafka producer/consumer proof of concept using node.
+
+## Prerequisites
+
+* `node`
+* `docker`
+
+## Running locally
+
+* `npm install` - installs npm dependencies.
+* `npm run producer` - starts producer.
+* `npm run consumer` - starts consumer.
--- src/producer/index.js
+++ src/producer/index.js
@@ -3,7 +3,14 @@ const { Kafka } = require("kafkajs")
 const main = async () => {
 
   const users = [
-    { id: 1, name: 'User 1' }
+    { id: 1, name: 'User 1' },
+    { id: 2, name: 'User 2' },
+    { id: 3, name: 'User 3' },
+    { id: 4, name: 'User 4' },
+    { id: 5, name: 'User 5' },
+    { id: 6, name: 'User 6' },
+    { id: 7, name: 'User 7' },
+    { id: 8, name: 'User 8' },
   ]
 
   const kafka = new Kafka({
